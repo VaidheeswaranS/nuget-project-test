ff334751b4d365be51c36c66e454b65b945135e1
Vaidheeswaran Srinivaan <vaidheeswaran.srinivaan@sage.com>
2024-03-06 13:49:46 +0530

    Add project files.

---
+++ nuget-project-test/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using nuget_project_test.contracts;
+
+namespace nuget_project_test
+{
+    public class Calculator: ICalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
